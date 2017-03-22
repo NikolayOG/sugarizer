edb80a9c5dcc60fb527da3ca3e625725fa3a3d44
NikolayOG <nikolay.o.gospodinov@gmail.com>
2017-03-22 21:52:40 +0000

    Just a simple mock-up showing the usage of APIs

---
+++ dashboard/js/script.js
@@ -0,0 +1,46 @@
+
+var journalEntries = 0;
+
+$.ajax({
+    url: "http://localhost/sugarizer/api/users/",
+})
+    .done(function (data) {
+        // var journalEntries = 0;
+        $("#numberOfUsers").text(data.length)
+        for (var userIndex = 0; userIndex < data.length; userIndex++) {
+            var userName = data[userIndex].name;
+            $("#usersAPI").append('<a href="#" class="userName list-group-item list-group-item-action">' + userName + "</a>")
+
+            // $(".users").find("select").append("<option " + "class=names" + ">" + data[userIndex].name + "</option>");
+            // // use the private_journal id
+            // $(".users").find("option:nth-child(" + userIndex+1 + ")").attr("id", data[userIndex].private_journal);
+            $.ajax({
+                url: "http://localhost/sugarizer/api/journal/" + data[userIndex].private_journal
+            })
+                .done(function (data) {
+                    journalEntries += data.length;
+                    $(".journal-entries").text(journalEntries);
+                });
+
+            // if (data[userIndex].private_journal)
+        }
+    });
+
+// $.ajax({
+//     url: "http://localhost/sugarizer/api/journal/"
+// })
+//     .done(function (data) {
+//         console.log(data.length)
+//     });
+
+
+
+$("#search-criteria").on("keyup", function() {
+    var g = $(this).val().toLowerCase();
+    $(".userName").each(function () {
+        var s = $(this).text().toLowerCase();
+        $(this).closest('.userName')[s.indexOf(g) !== -1 ? 'show' : 'hide']();
+    });
+})
+
+
